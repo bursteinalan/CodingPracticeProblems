2e23c410759215c66fb49e9403a4bbef23de2fae
Charisma Nguyen <main@Charismas-MacBook-Pro.local>
2019-07-08 20:21:11 -0400

    Sln P1

--- CharismaSolns.py
+++ CharismaSolns.py
@@ -1,4 +1,23 @@
-def problem1():
+def problem1(n):
+
+        #base case
+        if n <= 3:
+                return n
+        
+        #max perf squares
+        ans = n
+
+        #breaking down and finding min perf squares
+        for i in range(1,n+1):
+                
+                temp = i * i;
+                if temp > n:
+                        break
+                else:
+                        ans = min(ans, 1 +problem1(n-temp))
+
+        return ans;
+
 
 def main():
 	p1_1 = problem1(12)
