d769b5c9161bef277bb0bf208609d79e0628675b
burstein <alanbur@bu.edu>
2019-07-09 18:10:44 -0500

    problem 2

--- AlanSolns.py
+++ AlanSolns.py
@@ -22,11 +22,20 @@ def problem1(n):
 		results.append(min_num)
 	return results[n]
 
+def problem2(n):
+	'''
+	'''
+	a=1
 
 def main():
-	p1_1 = problem1(12)
-	p1_2 = problem1(13)
-	print('p1_1 should be 3 and is:',p1_1)
-	print('p1_2 should be 2 and is:',p1_2)
+	# p1_1 = problem1(12)
+	# p1_2 = problem1(13)
+	# print('p1_1 should be 3 and is:',p1_1)
+	# print('p1_2 should be 2 and is:',p1_2)
+    p2_1 = problem2("23")
+    p2_1_ans = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+    print('p2_1 should be',p2_1_ans,' and is:',p2_1)
+
 main()
+
 
--- CharismaSolns.py
+++ CharismaSolns.py
@@ -19,9 +19,14 @@ def problem1(n):
         return ans;
 
 
-def main():
-	p1_1 = problem1(12)
-	p1_2 = problem1(13)
-	print('p1_1 should be 3 and is:',p1_1)
-	print('p1_2 should be 2 and is:',p1_2)
+def problem2():
 
+def main():
+	# p1_1 = problem1(12)
+	# p1_2 = problem1(13)
+	# print('p1_1 should be 3 and is:',p1_1)
+	# print('p1_2 should be 2 and is:',p1_2)
+        p2_1 = problem2("23")
+        p2_1_ans = ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+        print('p2_1 should be ',p2_1_ans,' and is:',p2_1)
+main()
