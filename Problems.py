76fede9881f5169f808aa0b8634ad6bd212148a2
burstein <alanbur@bu.edu>
2019-07-10 19:18:36 -0500

    problem 3

--- Problems.py
+++ Problems.py
@@ -38,3 +38,28 @@ def problem2():
 	Although the above answer is in lexicographical order, your answer could be in any order you want.
 	'''
 
+def problem3():
+	'''
+	Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+	An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+	You may assume all four edges of the grid are all surrounded by water.
+
+	Example 1:
+
+	Input:
+	11110
+	11010
+	11000
+	00000
+
+	Output: 1
+	Example 2:
+
+	Input:
+	11000
+	11000
+	00100
+	00011
+
+	Output: 3
+	'''
